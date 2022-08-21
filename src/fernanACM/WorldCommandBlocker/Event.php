5522bb85d013fb0f631fdd6e36ad2f94a9d48659
fernanACM <fernandoclaverimontesinos@gmail.com>
2022-08-20 20:33:39 -0500

    Done

---
+++ src/fernanACM/WorldCommandBlocker/Event.php
@@ -0,0 +1,55 @@
+<?php
+
+#      _       ____   __  __ 
+#     / \     / ___| |  \/  |
+#    / _ \   | |     | |\/| |
+#   / ___ \  | |___  | |  | |
+#  /_/   \_\  \____| |_|  |_|
+# The creator of this plugin was fernanACM.
+# https://github.com/fernanACM
+
+namespace fernanACM\WorldCommandBlocker;
+
+use fernanACM\PluginUtils;
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+use fernanACM\WorldCommandBlocker\utils\PluginUtils;
+use fernanACM\WorldCommandBlocker\Loader;
+
+class Event implements Listener{
+
+    public function onPreCommand(PlayerCommandPreprocessEvent $event){
+        $player = $event->getPlayer();
+        $command = explode(" ", $event->getMessage())[0];
+        if(str_starts_with($command, "/")){
+            if(isset(Loader::blocker[$player->getWorld()->getDisplayName(])){
+                if(in_array(str_replace("/", "", $command), Loader::blocker[$player->getWorld()->getDisplayName()])){
+                    $event->cancel();
+                    $player->sendMessage(Loader::getMessage($player, "error-message"));
+                    PluginUtils::PlaySound($player, "mob.villager.no", 1, 1);
+                }
+            }
+        }elseif(str_starts_with($command, "./")){
+            if(isset(Loader::blocker[$player->getWorld()->getDisplayName(])){
+                if(in_array(str_replace("./", "", $command), Loader::blocker[$player->getWorld()->getDisplayName()])){
+                    $event->cancel();
+                    $player->sendMessage(Loader::getMessage($player, "error-message"));
+                    PluginUtils::PlaySound($player, "mob.villager.no", 1, 1);
+                }
+            }
+        }elseif(str_starts_with($command, "")){
+            if(isset(Loader::blocker[$player->getWorld()->getDisplayName(])){
+                if(in_array(str_replace("", "", $command), Loader::blocker[$player->getWorld()->getDisplayName()])){
+                    $event->cancel();
+                    $player->sendMessage(Loader::getMessage($player, "error-message"));
+                    PluginUtils::PlaySound($player, "mob.villager.no", 1, 1);
+                }
+            }
+        }
+    }
+}
