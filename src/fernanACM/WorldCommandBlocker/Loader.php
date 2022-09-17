24a8df0b44d917afef5c3152e151df6c33fa9e6e
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-09-17 16:58:53 -0500

    DONE

--- src/fernanACM/WorldCommandBlocker/Loader.php
+++ src/fernanACM/WorldCommandBlocker/Loader.php
@@ -28,6 +28,9 @@ class Loader extends PluginBase{
     public $blocker = [];
     # Instance
     public static $instance;
+    # CheckConfig
+    public const CONFIG_VERSION = "1.0.0";
+    public const LANGUANGE_VERSION = "1.0.0";
     # MultiLanguages
     public const LANGUAGES = [
         "eng", // English
@@ -42,6 +45,7 @@ class Loader extends PluginBase{
     public function onEnable(): void{
          # Config files
         $this->loadFiles();
+        $this->loadCheck();
         $this->loadEvents();
         # Instance
         self::$instance = $this;
@@ -49,15 +53,35 @@ class Loader extends PluginBase{
     
     public function loadFiles(){
          # Config files
-        @mkdir($this->getDataFolder() . "languages");
-        $this->saveResource("config.yml");
-        $this->config = new Config($this->getDataFolder() . "config.yml");
-        # Languages
-        foreach(Loader::LANGUAGES as $language){
-            $this->saveResource("languages/" . $language . ".yml");
+         @mkdir($this->getDataFolder() . "languages");
+         $this->saveResource("config.yml");
+         $this->config = new Config($this->getDataFolder() . "config.yml");
+         # Languages
+         foreach(Loader::LANGUAGES as $language){
+             $this->saveResource("languages/" . $language . ".yml");
+         }
+         $this->messages = new Config($this->getDataFolder() . "languages/" . $this->config->get("language") . ".yml");
+         $this->blocker = $this->config->getNested("Settings.blocked-commands", []);
+    }
+
+    public function loadCheck(){
+        # CONFIG
+        if((!$this->config->exists("config-version")) || ($this->config->get("config-version") != self::CONFIG_VERSION)){
+            rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_old.yml");
+            $this->saveResource("config.yml");
+            $this->getLogger()->critical("Your configuration file is outdated.");
+            $this->getLogger()->notice("Your old configuration has been saved as config_old.yml and a new configuration file has been generated. Please update accordingly.");
         }
-        $this->messages = new Config($this->getDataFolder() . "languages/" . $this->config->get("language") . ".yml");
-        $this->blocker = $this->config->getNested("Settings.blocked-commands", []);
+        # LANGUAGES
+        $data = new Config($this->getDataFolder() . "languages/" . $this->config->get("language") . ".yml");
+        if((!$data->exists("language-version")) || ($data->get("language-version") != self::LANGUANGE_VERSION)){
+            rename($this->getDataFolder() . "languages/" . $this->config->get("language") . ".yml", $this->getDataFolder() . "languages/" . $this->config->get("language") . "_old.yml");
+            foreach(Loader::LANGUAGES as $language){
+                $this->saveResource("languages/" . $language . ".yml");
+            }
+            $this->getLogger()->critical("Your ".$this->config->get("language").".yml file is outdated.");
+            $this->getLogger()->notice("Your old ".$this->config->get("language").".yml has been saved as ".$this->config->get("language")."_old.yml and a new ".$this->config->get("language").".yml file has been generated. Please update accordingly.");
+        }
     }
 
     public function loadEvents(){
